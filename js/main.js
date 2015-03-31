1278da4ed3aa2f9454bb0b1996d7aef1065dfee9
Andrew Lord <andrewlord1990@gmail.com>
2015-03-31 18:56:00 +0100

    Isotope filtering now working on projects.

--- js/main.js
+++ js/main.js
@@ -31,49 +31,18 @@ $('.navbar-collapse ul li a').click(function () {
 });
 
 function setupProjectsTabs() {
-    if ($('.isotopeWrapper').length) {
-        var $container = $('.isotopeWrapper');
-        var $resize = $('.isotopeWrapper').attr('id');
-        
+    var $container = $('.isotope').isotope({
+        itemSelector: '.project-item',
+        layoutMode: 'fitRows'
+    });
+
+    // bind filter button click
+    $('#projects nav .nav').on('click', 'a', function () {
+        var filterValue = $(this).attr('data-filter');
         $container.isotope({
-            itemSelector: '.isotopeItem',
-            resizable: false, // disable normal resizing
-            masonry: {
-                columnWidth: $container.width() / $resize
-            }
+            filter: filterValue
         });
-        $("a[href='#top']").click(function () {
-            $("html, body").animate({
-                scrollTop: 0
-            }, "slow");
-            return false;
-        });
-        $('.navbar').on('click', 'li a', function () {
-            $('.navbar .in').addClass('collapse').removeClass('in').css('height', '1px');
-        });
-        $('#projects nav a').click(function () {
-            $('#projects nav a').removeClass('current');
-            $(this).addClass('current');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 1000,
-                    easing: 'easeOutQuart',
-                    queue: false
-                }
-            });
-            return false;
-        });
-        $(window).smartresize(function () {
-            $container.isotope({
-                // update columnWidth to a percentage of container width
-                masonry: {
-                    columnWidth: $container.width() / $resize
-                }
-            });
-        });
-    }
+    });
 }
 
 $('document').ready(function () {
