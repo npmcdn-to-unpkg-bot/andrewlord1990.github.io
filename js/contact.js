9ff4f1595513386a814b94deb891315957b4475a
Andrew Lord <andrewlord1990@gmail.com>
2015-04-05 12:45:39 +0100

    Added floating labels to contact form. Styling of contact form.

--- js/contact.js
+++ js/contact.js
@@ -33,7 +33,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Your message has been sent. Thank you for contacting me, I will get back to you shortly.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -42,24 +42,17 @@ $(function() {
                 },
                 error: function() {
                     // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    $('#success').html("<div class='alert alert-warning'>");
+                    $('#success > .alert-warning').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
+                    $('#success > .alert-warning').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-warning').append('</div>');
                 },
             })
         },
         filter: function() {
             return $(this).is(":visible");
         },
-    });
-
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
     });
 });
 
