78bb8e21adbd22bb0fff6360f034dd7e0b424fd5
Andrew Lord <andrew@andrew-lord.co.uk>
2016-02-28 19:24:19 +0000

    Fix htmlproof command.

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 bundle exec jekyll build
-bundle exec htmlproof ./_site --check_html true --href-ignore "#" --file-ignore "*/bower_components/*"
+bundle exec htmlproof ./_site --href-ignore "#"
