eb056699e1ec41da0a8febe5324b37c28f69ffd3
Andrew Lord <andrew@andrew-lord.co.uk>
2016-03-17 22:58:54 +0000

    Trying to fix Travis build.

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 bundle exec jekyll build
-bundle exec htmlproof ./_site --href-ignore "#,https://uk.linkedin.com/in/andrewlord1990"
+bundle exec htmlproofer ./_site --href-ignore "#,https://uk.linkedin.com/in/andrewlord1990"
