803ee72357f5348e2a1e1c01b24df2ca27715281
Andrew Lord <andrew@andrew-lord.co.uk>
2016-05-20 21:06:05 -0700

    Added trace argument to build to find out why it is failing on travis.

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-bundle exec jekyll build
+bundle exec jekyll build --trace
 bundle exec htmlproofer ./_site --url-ignore "#,https://uk.linkedin.com/in/andrewlord1990"
 bootlint ./_site/*.html
