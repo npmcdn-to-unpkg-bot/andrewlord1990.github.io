262b666c59628a7c1b037949caa3d1f7cc984b83
Andrew Lord <andrew@andrew-lord.co.uk>
2016-05-25 18:36:47 +0100

    Disabled external link checking as just fails regularly.

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -5,14 +5,6 @@ function msg {
 	printf "\e[0;37m==> $1\e[0m\n"
 }
 
-IGNORE_HREFS=$(ruby -e 'puts %w{
-	www.wayfair.com/,
-	www.amazon.com/echo/,
-	play.google.com/music/,
-	uk.linkedin.com/in/andrewlord1990,
-	#
-}.map{|h| "/#{h}/"}.join(",")')
-
 export PROOF=true
 export NOKOGIRI_USE_SYSTEM_LIBRARIES=true
 
@@ -20,6 +12,6 @@ msg "Building..."
 bundle exec jekyll build --trace
 
 msg "Proofing..."
-bundle exec htmlproofer ./_site --url-ignore $IGNORE_HREFS  --check-html
+bundle exec htmlproofer ./_site --disable-external --allow-hash-href --check-html
 
 bootlint ./_site/*.html
